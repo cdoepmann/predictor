tschorsch/nstor

add nagles algorithm for bktap

src/tor/model/tor-bktap.cc
@@ -63,8 +63,11 @@ UdpChannel::Flush ()
 
 
 void
-UdpChannel::ScheduleFlush ()
+UdpChannel::ScheduleFlush (bool delay)
 {
+  if (m_flushQueue.size() == 0) {
+    return;
+  }
   if (m_flushQueue.size () > 1)
     {
       m_flushEvent.Cancel ();
@@ -72,7 +75,13 @@ UdpChannel::ScheduleFlush ()
     }
   else
     {
-      m_flushEvent = Simulator::Schedule (MilliSeconds (1), &UdpChannel::Flush, this);
+      if (!delay) {
+        m_flushEvent.Cancel();
+        m_flushEvent = Simulator::Schedule (MilliSeconds (1), &UdpChannel::Flush, this);
+      } else {
+        m_flushEvent.Cancel();
+        m_flushEvent = Simulator::Schedule (rttEstimator.baseRtt, &UdpChannel::Flush, this);
+      }
     }
 }
 
@@ -121,6 +130,7 @@ BktapCircuit::GetQueue (CellDirection direction)
     }
 }
 
+bool TorBktapApp::s_nagle = false;
 
 TorBktapApp::TorBktapApp ()
 {
@@ -376,6 +386,10 @@ TorBktapApp::ReceivedAck (Ptr<BktapCircuit> circ, CellDirection direction, FdbkC
       queue->DiscardUpTo (header.ack);
       Time rtt = queue->actRtt.EstimateRtt (header.ack);
       ScheduleRto (circ,direction,true);
+      if(!queue->PackageInflight()) {
+        Ptr<UdpChannel> ch = circ->GetChannel(direction);
+        ch->ScheduleFlush(false);
+      }
     }
   else
     {
@@ -580,11 +594,11 @@ TorBktapApp::FlushPendingCell (Ptr<BktapCircuit> circ, CellDirection direction, 
 
       ch->m_flushQueue.push (cell);
       int bytes_written = cell->GetSize ();
-      ch->ScheduleFlush ();
+      ch->ScheduleFlush (s_nagle && queue->PackageInflight());
 
       if (ch->SpeaksCells ())
         {
-          ScheduleRto (circ,direction);
+          ScheduleRto (circ,direction,true);
         }
       else
         {
@@ -744,6 +758,12 @@ TorBktapApp::DoDispose (void)
   baseCircuits.clear ();
   channels.clear ();
   Application::DoDispose ();
+}
+
+void
+TorBktapApp::SetNagle (bool nagle)
+{
+  s_nagle = nagle;
 }
 
 } //namespace ns3

cdoepmann/predictor

(cherry-picked from 29a39190f92a10f8493d43b9d8b78b55ea553eb6)

src/tor/model/tor-bktap.cc
@@ -129,8 +129,11 @@ UdpChannel::Flush ()
 
 
 void
-UdpChannel::ScheduleFlush ()
+UdpChannel::ScheduleFlush (bool delay)
 {
+  if (m_flushQueue.size() == 0) {
+    return;
+  }
   if (m_flushQueue.size () > 1)
     {
       m_flushEvent.Cancel ();
@@ -138,7 +141,13 @@ UdpChannel::ScheduleFlush ()
     }
   else
     {
-      m_flushEvent = Simulator::Schedule (MilliSeconds (1), &UdpChannel::Flush, this);
+      if (!delay) {
+        m_flushEvent.Cancel();
+        m_flushEvent = Simulator::Schedule (MilliSeconds (1), &UdpChannel::Flush, this);
+      } else {
+        m_flushEvent.Cancel();
+        m_flushEvent = Simulator::Schedule (rttEstimator.baseRtt, &UdpChannel::Flush, this);
+      }
     }
 }
 
@@ -187,6 +196,7 @@ BktapCircuit::GetQueue (CellDirection direction)
     }
 }
 
+bool TorBktapApp::s_nagle = false;
 
 TorBktapApp::TorBktapApp ()
 {
@@ -478,6 +488,10 @@ TorBktapApp::ReceivedAck (Ptr<BktapCircuit> circ, CellDirection from_direction, 
       queue->DiscardUpTo (header.ack);
       Time rtt = queue->actRtt.EstimateRtt (header.ack);
       ScheduleRto (circ,from_direction,true);
+      if(!queue->PackageInflight()) {
+        Ptr<UdpChannel> ch = circ->GetChannel(from_direction);
+        ch->ScheduleFlush(false);
+      }
   }
 else
     {
@@ -808,11 +822,11 @@ TorBktapApp::FlushPendingCell (Ptr<BktapCircuit> circ, CellDirection to_directio
       cout << Simulator::Now() << " " << GetNodeName() << " pushing pending " << format_packet(cell) << endl;
       ch->m_flushQueue.push (cell);
       int bytes_written = cell->GetSize ();
-      ch->ScheduleFlush ();
+      ch->ScheduleFlush (s_nagle && queue->PackageInflight());
 
       if (ch->SpeaksCells ())
         {
-          ScheduleRto (circ,to_direction);
+          ScheduleRto (circ,to_direction,true);
         }
       else
         {
@@ -1013,6 +1027,12 @@ TorBktapApp::DoDispose (void)
   baseCircuits.clear ();
   channels.clear ();
   Application::DoDispose ();
+}
+
+void
+TorBktapApp::SetNagle (bool nagle)
+{
+  s_nagle = nagle;
 }
 
 } //namespace ns3